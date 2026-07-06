8a7a6e433ec09ec79d0cb7e5607db3e54db89db2
stannis-b <donghyeoni535@gmail.com>
2026-07-06 02:10:58 -0500

    Add files via upload
    Japanese language game

---
+++ application.rb
@@ -0,0 +1,19 @@
+Rails.application.configure do
+  config.enable_reloading = true
+  config.eager_load = false
+  config.consider_all_requests_local = true
+  config.server_timing = true
+
+  config.action_controller.perform_caching = false
+
+  config.active_support.deprecation = :log
+  config.active_support.disallowed_deprecation = :raise
+  config.active_support.disallowed_deprecation_warnings = []
+
+  config.active_record.migration_error = :page_load
+  config.active_record.verbose_query_logs = true
+
+  config.action_view.annotate_rendered_view_with_filenames = true
+
+  config.hosts.clear
+end
---
+++ development.rb
@@ -0,0 +1,9 @@
+Rails.application.configure do
+  config.enable_reloading = false
+  config.eager_load = true
+  config.consider_all_requests_local = false
+  config.action_controller.perform_caching = true
+  config.log_level = :info
+  config.active_record.migration_error = :page_load
+  config.hosts.clear
+end
