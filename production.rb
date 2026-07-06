966dc6385d8c71e97bc8449101be24eb5a9ec2ba
stannis-b <donghyeoni535@gmail.com>
2026-07-06 02:33:58 -0500

    Delete production.rb

--- production.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.configure do
-  config.enable_reloading = false
-  config.eager_load = false
-  config.consider_all_requests_local = true
-  config.action_controller.perform_caching = false
-  config.hosts.clear
-end
